51cbc4521994b82ba9776df0a5f4e0a1bd52e0a8
Justin Barclay <git@justinbarclay.ca>
2023-08-04 21:50:44 -0700

    Add dirlocals

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((eval . (add-to-list 'load-path (expand-file-name ".")))))
+ (emacs-lisp-mode . ((fill-column . 100)
+                     (indent-tabs-mode . nil)
+                     (sentence-end-double-space . nil)
+                     (elisp-lint-indent-specs . ((describe . 1)
+                                                 (it . 1))))))
