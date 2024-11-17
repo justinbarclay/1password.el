26e27a2ef39fbf0ec4d5494c65a6d5afe8b5ac4b
Justin Barclay <git@justinbarclay.ca>
2024-11-16 16:05:56 -0800

    feat: switch to parsing 1password responses directly
    This feature means we no longer need to create and parse a buffer when
    dealing with 1password input. This makes it more difficult for secret
    information, like passwords from being easily discovered in the event
    of a `*1password*` buffer sticking around.

--- 1password-lib.el
+++ 1password-lib.el
@@ -35,7 +35,7 @@ output of the call to the 1Password CLI.  By default, this is
 
 (cl-defun 1password--execute-in-buffer-async (args
                                               &optional
-                                              (buffer-reader-fn #'json-parse-buffer)
+                                              (process-parse-fn #'json-parse-string)
                                               (buffer-name "*1password*"))
   "Run the 1password executable with `ARGS' and return a promise.
 
@@ -45,24 +45,21 @@ output of the call to the 1Password CLI.  By default, this is
 
 `BUFFER-NAME' is the name of the buffer that will house the
 1Password process and that 1Password will dump its output too"
-  (with-current-buffer (get-buffer-create buffer-name)
-    (let* ((qualifed-executable (executable-find 1password-executable))
-           (promise (aio-promise))
-           (sentinel-fn (lambda (process event)
-                          (let ((data (with-current-buffer (process-buffer process)
-                                                   (special-mode)
-                                                   (goto-char (point-min))
-                                                   (funcall buffer-reader-fn))))
-                            (aio-resolve promise (lambda () data))))))
-      (unless qualifed-executable
-        (error (format "Unable to find 1Password CLI '%s'" 1password-executable)))
-      (read-only-mode -1)
-      (erase-buffer)
-      (make-process :name "1password"
-                    :buffer buffer-name
-                    :command (append (list 1password-executable "--no-color") (split-string args " "))
-                    :sentinel sentinel-fn)
-      promise)))
+  (let* ((qualifed-executable (executable-find 1password-executable))
+         (promise (aio-promise))
+         (op-response nil)
+         (filter-fn (lambda (process string)
+                            (setq op-response (concat op-response string))))
+         (sentinel-fn (lambda (process event)
+                        (let ((data (funcall process-parse-fn op-response)))
+                          (aio-resolve promise (lambda () data))))))
+    (unless qualifed-executable
+      (error (format "Unable to find 1Password CLI '%s'" 1password-executable)))
+    (make-process :name "1password"
+                  :command (append (list 1password-executable "--no-color") (split-string args " "))
+                  :filter filter-fn
+                  :sentinel sentinel-fn)
+    promise))
 
 ;; Local Variables:
 ;; End:
